2d0d59835d3b307cbe7431b17eda78c4f761612f
Cydnie Seelenbinder <cydalexand@gmail.com>
2021-01-13 22:36:39 +0100

    18 more.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews
 
+  validates :address, length: { minimum: 1 }
+  validates :name, length: { minimum: 1 }
   validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
 end
