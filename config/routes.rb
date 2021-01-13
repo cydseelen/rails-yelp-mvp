ca3c33b10b9348a872ab0e42897bfd4420eed5aa
Cydnie Seelenbinder <cydalexand@gmail.com>
2021-01-13 22:08:49 +0100

    22 more.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants
+
   get '/restaurants', to: 'restaurants#index'
   get '/restaurants/:id', to: 'restaurants#show'
   get '/restaurants/new', to: 'restaurants#new'
   post '/restaurants', to: 'restaurants#create'
+
+  get '/reviews/new', to: 'restaurants#new'
+  post '/reviews', to: 'restaurants#create'
 end
