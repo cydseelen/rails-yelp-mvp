da0039549f79598e2712812794e77e70eb956ebf
Cydnie Seelenbinder <cydalexand@gmail.com>
2021-01-14 19:27:05 +0100

    12 more to go.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,9 +2,9 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
   validates :content, length: { minimum: 2 }
   validates :content, presence: true
 end
 
-# numericality: { only_integer: true }
 # :inclusion => 0..5,
