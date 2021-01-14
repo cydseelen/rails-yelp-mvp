844ff9c091fb714e9733d03148f8ac62db9d0ede
Cydnie Seelenbinder <cydalexand@gmail.com>
2021-01-14 19:30:24 +0100

    Models and routing complete.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,8 @@ class Review < ApplicationRecord
 
   validates :rating, presence: true
   validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: (0..5),
+    message: "Invalid entry. Please choose a value between 0 and 5." }
   validates :content, length: { minimum: 2 }
   validates :content, presence: true
 end
